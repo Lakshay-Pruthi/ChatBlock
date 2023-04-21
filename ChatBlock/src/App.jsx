5525bdaa6a7a281c997b678e7aecca352f1cb2a6
Lakshay Pruthi <lakshay47pruthi16@gmail.com>
2023-04-21 23:33:25 +0530

    web3 provider added

--- ChatBlock/src/App.jsx
+++ ChatBlock/src/App.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from 'react';
+
+import Web3 from 'web3'
 
 import './App.css'
 
 function App() {
 
+  const provider = window.ethereum;
+  const [web3, setWeb3] = useState(null);
+  const [userAccount, setUserAccount] = useState(null);
+
+  if (provider) {
+    useEffect(() => {
+      function loadWeb3() {
+        setWeb3(new Web3(provider));
+        ethereum.request({ method: 'eth_requestAccounts' });
+      }
+
+      provider && loadWeb3();
+    }, [provider]);
+  }
 
   return (
     <>
