1f25e3b61e107af712017ee992abf512bce73cda
Lakshay Pruthi <lakshay47pruthi16@gmail.com>
2023-04-22 12:33:19 +0530

    added nested routes

--- ChatBlock/src/App.jsx
+++ ChatBlock/src/App.jsx
@@ -12,6 +12,7 @@ import './App.css'
 import Home from './Pages/Home';
 import Rooms from './Pages/Rooms';
 import About from './Pages/About';
+import Main from './Pages/Main';
 
 function App() {
 
@@ -77,9 +78,11 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Rooms' element={<Rooms />} />
-          <Route path='/About' element={<About />} />
+          <Route path='/' element={<Main />} >
+            <Route index element={<Home />} />
+            <Route path='/Rooms' element={<Rooms />} />
+            <Route path='/About' element={<About />} />
+          </Route>
         </Routes>
 
       </BrowserRouter>
