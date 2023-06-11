1e4416a4a68158028f3bbd7efe0f750090c6b79d
Lakshay Pruthi <lakshay47pruthi16@gmail.com>
2023-06-11 22:07:08 +0530

    context api integrated

--- Sandesha/src/Pages/Home.jsx
+++ Sandesha/src/Pages/Home.jsx
@@ -2,14 +2,7 @@ import Form from "../Components/Form";
 import logo from '../assets/Logo2.jpg'
 
 
-<<<<<<< HEAD
 function Home() {
-=======
-function Home(props) {
-    const { web3, contract, userAccount, provider } = props;
-
-
->>>>>>> 960238eb0f1200269b6cc74dc74fa4eed5fc3052
     return (
         <>
             <div className="container">
@@ -19,11 +12,7 @@ function Home(props) {
                     <p>- Decentralized chat platform</p>
                     <h2>Welcome</h2>
                 </div>
-<<<<<<< HEAD
                 <Form />
-=======
-                <Form web3={web3} contract={contract} userAccount={userAccount} provider={provider} />
->>>>>>> 960238eb0f1200269b6cc74dc74fa4eed5fc3052
 
             </div>
         </>
